da2c6d31e98026fbf6c7f52298e123de761a5f1f
Alberto <karcernucle4rthrone@gmail.com>
2025-01-22 11:22:09 +0100

    feat(BOWLING_PROGRAM): rehacer y añadir progreso

--- src/scoreCard.py
+++ src/scoreCard.py
@@ -1,13 +1,4 @@
-#ScoreCard o la clase donde almacenamos los datos de la tarjeta de pines y calculamos el puntaje total.
 
-class ScoreCard:
 
-    Spare = "/"
-    No_points = "-"
-    Strike = "X"
-    Max_frames = 10
-    Max_rolls = 22
-    
-    
 
-    
+
